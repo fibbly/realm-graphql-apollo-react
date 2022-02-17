bfcb295bc3ce7145060a7348165cdc0d982ce728
Angel Perez <fibble@angels-mbp-2.lan>
2022-02-17 14:49:37 -0500

    initial commit with realm app id

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 // Apollo
 import {
-  ApolloProvider,
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
+	ApolloProvider,
+	ApolloClient,
+	HttpLink,
+	InMemoryCache,
 } from "@apollo/client";
 // Realm
 import * as Realm from "realm-web";
@@ -23,47 +23,47 @@ import App from "./App";
 // 5. Deploy your changes
 //
 // Once your app is set up, replace the value of APP_ID with your App ID
-export const APP_ID = "<Your App ID>";
+export const APP_ID = process.env.REACT_APP_APP_ID;
 
 // Connect to your MongoDB Realm app
 const app = new Realm.App(APP_ID);
 
 // Gets a valid Realm user access token to authenticate requests
 async function getValidAccessToken() {
-  // Guarantee that there's a logged in user with a valid access token
-  if (!app.currentUser) {
-    // If no user is logged in, log in an anonymous user. The logged in user will have a valid
-    // access token.
-    await app.logIn(Realm.Credentials.anonymous());
-  } else {
-    // An already logged in user's access token might be stale. To guarantee that the token is
-    // valid, we refresh the user's custom data which also refreshes their access token.
-    await app.currentUser.refreshCustomData();
-  }
-  
-  return app.currentUser.accessToken;
+	// Guarantee that there's a logged in user with a valid access token
+	if (!app.currentUser) {
+		// If no user is logged in, log in an anonymous user. The logged in user will have a valid
+		// access token.
+		await app.logIn(Realm.Credentials.anonymous());
+	} else {
+		// An already logged in user's access token might be stale. To guarantee that the token is
+		// valid, we refresh the user's custom data which also refreshes their access token.
+		await app.currentUser.refreshCustomData();
+	}
+
+	return app.currentUser.accessToken;
 }
 
 // Configure the ApolloClient to connect to your app's GraphQL endpoint
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`,
-    // We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
-    // The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
-    // access token before sending the request.
-    fetch: async (uri, options) => {
-      const accessToken = await getValidAccessToken();
-      options.headers.Authorization = `Bearer ${accessToken}`;
-      return fetch(uri, options);
-    },
-  }),
-  cache: new InMemoryCache()
+	link: new HttpLink({
+		uri: `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`,
+		// We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
+		// The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
+		// access token before sending the request.
+		fetch: async (uri, options) => {
+			const accessToken = await getValidAccessToken();
+			options.headers.Authorization = `Bearer ${accessToken}`;
+			return fetch(uri, options);
+		},
+	}),
+	cache: new InMemoryCache(),
 });
 
 // Wrap your app with an ApolloProvider that provides the client
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root")
 );
